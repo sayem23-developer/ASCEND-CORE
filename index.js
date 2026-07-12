effe28ba48b3be60c982f06d903fc150458b9d2f
sayem23-developer <adminsayeem@gmail.com>
2026-07-13 03:15:24 +0600

    Add Render web server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 
 const client = new Client({
   intents: [
@@ -17,4 +18,18 @@ client.commands = new Collection();
 require("./handlers/commandHandler")(client);
 require("./handlers/eventHandler")(client);
 
+// Render Web Service Port
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("ASCEND BOT is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
+
+// Login Discord Bot
 client.login(process.env.TOKEN);
